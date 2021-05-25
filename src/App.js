d7b56cbdd3fe814fd96e47d358bb950ed04c017b
alfiyaenikeeva <alfiya.en@justiceteam-it.com>
2021-05-25 12:24:28 +0300

    create card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+import Card from './components/Card/Card';
+
+import image from './assets/images/svg/ice cream.png';
+
 function App() {
   return (
     <div className="App">
       App
+        <Card
+          text="Snow Tender Ice Cream"
+          image={image}
+          price="243.00"
+        />
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Card.scss';
+
+const Card = ({text, image, price}) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={image} alt="ice-cream" />
+      </div>
+      <div className="card-info">
+        <p className="card-name">{text}</p>
+        <span className="card-price">${price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;

a64cb6a6824542a62469979d9d6fd8276e3b9fb6
alfiyaenikeeva <alfiya.en@justiceteam-it.com>
2021-05-26 10:24:51 +0300

    refactor change styled components to scss

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import HeaderButton from '../HeaderButton/HeaderButton';
 
 import user from "../../../assets/images/svg/user.svg";
 import cart from "../../../assets/images/svg/cart.svg";
-
-const StyledNav = styled.nav`
-  display: flex;
-
-  @media (max-width: 480px) {
-    flex-flow: column nowrap;
-    align-items: center;
-    justify-content: space-around;
-    position: absolute;
-    width: 100%;
-    top: 92px;
-    right: 0;
-    transform: ${({ open }) => open ? 'translateY(0)' : 'translateY(-100%)'};
-    opacity: ${({ open }) => open ? '1' : '0'};
-    background: white;
-    border: 1px solid #F8F8F8;
-    border-radius: 0 0 10px 10px;
-    transition: transform 0.3s ease-in-out, opacity 0.2s ease-in-out;
-  }
-`;
+import './Menu.scss';
 
 const Menu = ({ open }) => (
-  <StyledNav
-    open={open}
-    className="header-nav"
+  <div
+    className={open ? "header-nav open" : "header-nav"}
   >
     <HeaderButton
       img={user}
@@ -42,7 +21,7 @@ const Menu = ({ open }) => (
       text="Cart"
       notifications={4}
     />
-  </StyledNav>
+  </div>
 );
 
 export default Menu;

01cf4fb1a2a82a566af954a7b8e2c13301f8b79d
alfiyaenikeeva <alfiya.en@justiceteam-it.com>
2021-05-26 14:07:50 +0300

    add main nav

---
+++ src/components/MainNav/MainNav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './MainNav.scss';
+
+const MainNav = () => (
+  <div className="main-nav">
+    <div className="container">
+      <span>Main page</span>
+      <span> / </span>
+      <span>Product card</span>
+    </div>
+  </div>
+)
+
+export default MainNav;

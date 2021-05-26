5e716ea9829121f908e41ffe485748bb1d10f3cc
alfiyaenikeeva <alfiya.en@justiceteam-it.com>
2021-05-26 12:45:18 +0300

    add product card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import ProductCard from './components/ProductCard/ProductCard';
 
 import './App.css';
 
 const App = () => (
   <div className="App">
     <Header />
+    <ProductCard />
     <Main />
     <Footer />
   </div>
---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import image from '../../assets/images/png/product ice cream.png';
+import './ProductCard.scss';
+
+const ProductCard = () => (
+  <div className="product-card">
+    <div className="container">
+      <div className="product-card-image">
+        <img src={image} alt="product" />
+      </div>
+      <div className="product-card-info">
+        <div className="product-card-code">SKU: BXD100BLK</div>
+        <h2 className="product-card-title">Snow Tender Ice Cream</h2>
+        <p className="product-card-subtitle">Description: </p>
+        <p className="product-card-descr">
+          Chocolate ice cream has a bright, rich and refreshing taste of the ingredient it contains. Thanks to liquid nitrogen shock freezing (-193°C), which freezes all the ingredients instantly and gives the ice cream an amazingly delicate texture, all the flavors, vitamins and nutrients are preserved by 99%.
+          <br />
+          <br />
+          Blast freezing with liquid nitrogen (-193°C), which freezes all the ingredients instantly and gives the ice cream an amazingly delicate texture, preserving all the flavors, vitamins and nutrients by 99%.
+        </p>
+        <div className="product-card-flex">
+          <span className="product-card-price">$243.00</span>
+
+          <div className="product-card-input">
+            <div className="product-card-btn"></div>
+            <span>1</span>
+            <div className="product-card-btn"></div>
+          </div>
+        </div>
+
+        <button className="product-card-button">
+          <span></span>
+          <span>Add to cart</span>
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default ProductCard;

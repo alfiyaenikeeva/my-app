64c2962b866a4c41aaad0a5b0c9e847eb2583135
alfiyaenikeeva <alfiya.en@justiceteam-it.com>
2021-05-27 13:43:46 +0300

    add spinner and notification to productCardBtn

--- src/components/ProductCard/ProductCardBtn/ProductCardBtn.js
+++ src/components/ProductCard/ProductCardBtn/ProductCardBtn.js
@@ -1,15 +1,42 @@
 import React from "react";
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import './ProductCardBtn.scss';
 
-const ProductCardBtn = ({ addToCart }) => (
-  <button
-    onClick={addToCart}
-    className="product-card-button"
-  >
-    <span></span>
-    <span>Add to cart</span>
-  </button>
-);
+const ProductCardBtn = ({ addToCart }) => {
+  const isLoading = false;
+  const isAdded = false;
+
+  const ButtonInner = () => (
+    <>
+      <span></span>
+      <span>Add to cart</span>
+    </>
+  );
+
+  const ButtonNotification = () => (
+    <span className="product-card-button-msg">
+      <span></span>
+      <span>Added to cart</span>
+    </span>
+  );
+
+  return (
+    <div className="product-card-button-wrapper">
+      <button
+        onClick={addToCart}
+        className="product-card-button"
+      >
+        {
+          isLoading ? <CircularProgress size={20} color="inherit" /> : <ButtonInner />
+        }
+      </button>
+      {
+        isAdded ? <ButtonNotification /> : null
+      }
+    </div>
+  );
+};
 
 export default ProductCardBtn;
